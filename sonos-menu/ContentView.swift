ae962018986a01308a1f8c4082362cb2eb3d7ac3
Gustav Junedahl <gustav.junedahl@gmail.com>
2026-07-24 17:41:06 +0200

    Initial Commit

---
+++ sonos-menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sonos-menu
+//
+//  Created by Gustav Junedahl on 2026-07-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sonos-menu/sonos_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sonos_menuApp.swift
+//  sonos-menu
+//
+//  Created by Gustav Junedahl on 2026-07-24.
+//
+
+import SwiftUI
+
+@main
+struct sonos_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
